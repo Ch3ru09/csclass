6c894ded7f46293e2d0b5d896e534b1f8cb940c7
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2021-11-09 21:33:03 -0500

    idk

--- HTML/memory/memory.js
+++ HTML/memory/memory.js
@@ -1,1 +1,17 @@
 const main = document.getElementById('main')
+
+const names = ['chat', 'cheval', 'chien', 'cochon', 'lapin', 'poule']
+
+names.map(element => {
+  return {
+    animal: element,
+  }
+})
+
+function ranInt(min, max) {
+  min * (Math.random)
+}
+
+
+
+
