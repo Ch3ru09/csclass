713699e999e7512cc5057aac7b863592c9911d0c
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2021-11-09 23:21:35 -0500

    eh

--- HTML/memory/memory.js
+++ HTML/memory/memory.js
@@ -1,17 +1,29 @@
 const main = document.getElementById('main')
+import md5 from './src/md5.js'
 
 const names = ['chat', 'cheval', 'chien', 'cochon', 'lapin', 'poule']
 
 names.map(element => {
+  for (let i = 0; i < 2; i++) {
+
+  }
   return {
     animal: element,
   }
 })
 
 function ranInt(min, max) {
-  min * (Math.random)
+  return min + Math.floor(Math.random() * (max - min + 1))
 }
 
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/{}$[]';
 
-
-
+function ranString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+}
