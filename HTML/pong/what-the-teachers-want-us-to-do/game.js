bd139610a7bda66c3c20e1dd191ef8f69d230894
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-01-09 19:25:06 -0500

    df

--- HTML/pong/what-the-teachers-want-us-to-do/game.js
+++ HTML/pong/what-the-teachers-want-us-to-do/game.js
@@ -2,13 +2,13 @@ var game = document.getElementById('game');
 var ctx = game.getContext("2d");
 const W = game.width;
 const H = game.height;
-const pW = 100
-const pH = 15
-var pX = (W - pW)/2
-const pSpeed = 7
 var rightPressed = false
 var leftPressed = false
+var shiftPressed = false
 let play = false
+
+paddle = new Paddle()
+
 
 numballs = document.getElementById('nbballs').value
 balls = []
@@ -21,12 +21,16 @@ function ranint(min, max) {
   return min + Math.floor(Math.random()*(max-min+1))
 }
 
+function ranreal(min, max) {
+  return min + (Math.random()*(max -min+1))
+}
+
 function Ball() {
   this.r = ranint(6, 12);
-	this.x = ranint(r, W-r);
-	this.y = ranint(r, -H+r);
-	this.dx = 5;
-	this.dy = 5;
+	this.x = ranint(this.r, W-this.r);
+	this.y = ranint(this.r, (H-this.r)/2);
+	this.dx = ranreal(-5, -1);
+	this.dy = ranreal(-5, 5);
 	this.color = 'hsl('+(Math.random()*360)+',90%,50%)';
 	
 	this.draw = () => {
@@ -37,7 +41,7 @@ function Ball() {
 	} 
 	
 	this.update = () => {
-		this.x += this.dy;
+		this.x += this.dx;
 		this.y += this.dy;
 		if (this.x > W - this.r) {
 			this.x = W - this.r;
@@ -63,41 +67,94 @@ document.addEventListener("keyup", keyUpHandler, false);
 function keyDownHandler(e) {
   if (e.key == "Right" || e.key == "ArrowRight" || e.key == 'd') {
     rightPressed = true;
-  }
-  else if (e.key == "Left" || e.key == "ArrowLeft" || e.key == 'a') {
+  } else if (e.key == "Left" || e.key == "ArrowLeft" || e.key == 'a') {
     leftPressed = true;
+  } else if (e.key == 'Shift') {
+    shiftPressed = true
+  } else if (e.key == 'r') {
+    reset()
   }
 }
 
 function keyUpHandler(e) {
   if (e.key == "Right" || e.key == "ArrowRight" || e.key == 'd') {
     rightPressed = false;
-  }
-  else if (e.key == "Left" || e.key == "ArrowLeft" || e.key == 'a') {
+  } else if (e.key == "Left" || e.key == "ArrowLeft" || e.key == 'a') {
     leftPressed = false;
+  } else if (e.key == 'Shift') {
+    shiftPressed = false
   }
 }
 
-function positionPaddle() {
-  if (rightPressed == true || leftPressed == true) return
-  
-  else if (rightPressed == true) {
+function Paddle() {
+  this.pW = 150
+  this.pH = 15
+  this.pX = (W - this.pW)/2
+  this.pSpeed = 20
+
+  this.drawPaddle = () => {
+    ctx.beginPath();
+    ctx.rect(this.pX, H - this.pH, this.pW, this.pH);
+    ctx.fillStyle = "#aaf";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  this.updatePaddle = () => {
+    shiftPressed == true
+      ? this.pSpeed = 7
+      : this.pSpeed  = 20
+    if (rightPressed == true && leftPressed == true) return this.drawPaddle()
     
-  } else if (leftPressed == true) {
-
+    else if (rightPressed == true) {
+      if (this.pX+this.pSpeed < W-this.pW) {
+        this.pX += this.pSpeed
+      } else {
+        this.pX = W-this.pW
+      }
+    } else if (leftPressed == true) {
+      if (this.pX-this.pSpeed > 0) {
+        this.pX -= this.pSpeed
+      } else {
+        this.pX = 0
+      }
+    }
+    this.drawPaddle()
   }
 }
 
-function drawPaddle() {
-  ctx.beginPath();
-  ctx.rect(pX, H - pH, pW, pH);
-  ctx.fillStyle = "#aaf";
-  ctx.fill();
-  ctx.closePath();
-} 
+function reset() {
+  balls = []
+  numballs = document.getElementById('nbballs').value
+  const pW = 150
+  const pH = 15
+  var pX = (W - pW)/2
+  let pSpeed = 20
+  var rightPressed = false
+  var leftPressed = false
+  var shiftPressed = false
+  let play = false
+  for(let i = 0; i < numballs; i++) {
+    balls.push(new Ball())
+  }
+}
 
 function animate() {
-  positionPaddle()
-  drawPaddle();
+  ctx.clearRect(0, 0, W, H);
+  paddle.updatePaddle()
+  
+  balls.forEach(ball => {
+    ball.update()
+    // balls.forEach(ball2 => {
+    //   detectCollision(ball, ball2)
+    //     .then(res => {
+    //       if (res[0]) {
+    //         ajustPos(ball, ball2, res[1])
+    //         resolve(ball, ball2)
+    //       }
+    //     })
+    // })
+  })
+
 	requestAnimationFrame(animate);
-}
+} animate()
